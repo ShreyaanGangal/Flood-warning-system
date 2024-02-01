d6436681fc828d467d7d489bd5726c045a945e1b
Shreyaan <sg2113@cam.ac.uk>
2024-02-01 21:15:06 +0000

    creates a list of number of rivers and number of monitoring stations, not running

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,12 +6,27 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+#from .utils import sorted_by_key  # noqa
 
 
 from floodsystem.stationdata import build_station_list
 
-def rivers_greatest_no_monitoring_stations():
+def rivers_by_station_number(stations, N):
     stations = build_station_list()
+    rivers = ()
+    number_of_stations = []
+    # create a list of all the rivers and the number of monitoring stations on each
+    for station in stations:
+        if station.river in rivers:
+            number_of_stations[rivers.index(station.river)] = number_of_stations[rivers.index(station.river)] + 1
+        else:
+           rivers.append(station.river)
+           number_of_stations[rivers.index(station.river)] = number_of_stations[rivers.index(station.river)] + 1
+    tuple_rivers_number = ()
+    for i in len(number_of_stations):
+        tuple_rivers_number.append(rivers(i), number_of_stations[i])
     
+    return tuple_rivers_number
+
+
 
