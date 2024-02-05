aa908676eb3236b5a056380ca29dca1ac9edad89
nat-inn <ntqi2@cam.ac.uk>
2024-02-05 22:43:43 +0000

    Task 1D Success

--- Task1D.py
+++ Task1D.py
@@ -6,12 +6,14 @@ def run():
     """prints how many rivers have at least one monitoring station and prints the first 10 of these rivers in alphabetical order"""
     stations = build_station_list()
     river_container = rivers_with_station(stations)
+    print(f"{len(river_container)} stations.\n")
+    print(f"{river_container[0:10]}\n")
     
-    print(f"{len(river_container)} stations.")
-    print(river_container[0:10])
-
-    """prints """
-
+    """prints the names of the stations located on the 'River Aire', 'River Cam', and 'River Thames' in alphabetical order"""
+    dict_of_rivers = stations_by_river(stations)
+    print(f"{dict_of_rivers['River Aire']}\n")
+    print(f"{dict_of_rivers['River Cam']}\n")
+    print(f"{dict_of_rivers['River Thames']}\n")
 
 if __name__ == '__main__':
     run()
