18aca54f9dd1c480c6ac104d931a540c06528471
Shreyaan <sg2113@cam.ac.uk>
2024-02-05 11:35:23 +0000

    Task 1F completed with def run() put in

--- Task1F.py
+++ Task1F.py
@@ -3,26 +3,17 @@ from floodsystem.station import inconsistent_typical_range_stations
 from floodsystem.station import MonitoringStation
 
 
-stations = build_station_list()
-
-x = inconsistent_typical_range_stations(stations)
-
-''''list_inconsistent_stations = []
-print (x)
-for i in x:
-    if 'Station name' in i:
-         list_inconsistent_stations.append(i['Station name'])
-print (list_inconsistent_stations)'''
 
 
-''''for e in data["items"]:
-        # Extract town string (not always available)
-        town = None
-        if 'town' in e:
-            town = e['town']'''''
-
+def run():
+    stations = build_station_list()
+    x = inconsistent_typical_range_stations(stations)
 #print (x)
-list_inconsistent_stations = []
-for station in x:
-    list_inconsistent_stations.append(station.name)
-print (sorted(list_inconsistent_stations))
+    list_inconsistent_stations = []
+    for station in x:
+        list_inconsistent_stations.append(station.name)
+    print (sorted(list_inconsistent_stations))
+
+if __name__ == "__main__":
+    run()
+
