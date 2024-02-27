729160e2b84b4f1ac04ace6d8c5b4ab948ef06d6
nat-inn <ntqi2@cam.ac.uk>
2024-02-27 14:17:44 +0000

    Task 2C Success

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -4,26 +4,21 @@ from .utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     """Returns a list of tuples that hold a station at which the latest relative water level is over 'tol' and the relative water level at the station."""
-    list = []
+    list_1 = []
     update_water_levels(stations)
     for station in stations:
         if station.relative_water_level() is not None:
             if station.relative_water_level() > tol:
-                list.append((station, station.relative_water_level()))
-    sorted_list_by_rwl = sorted_by_key(list, 1, reverse= True)
+                list_1.append((station, station.relative_water_level()))
+    sorted_list_by_rwl = sorted_by_key(list_1, 1, reverse= True)
     return sorted_list_by_rwl
 
 
 def stations_highest_rel_level(stations, N):
     """Returns a list of N stations at which the water level, relative to the typical range, is highest."""
-    list = []
+    list_1 = []
     update_water_levels(stations)
     for station in stations:
         if station.relative_water_level() is not None:
-            list.append((station, station.relative_water_level()))
-    sorted_list = sorted_by_key(list, 1, reverse= True)
-    
-    N_sorted_list = []
-    for target in sorted_list: 
-        N_sorted_list.append(target[0:N])
-    return N_sorted_list
+            list_1.append((station, station.relative_water_level()))
+    return sorted_by_key(list_1, 1, reverse= True)[0:N]
--- floodsystem/station.py
+++ floodsystem/station.py
@@ -49,7 +49,6 @@ class MonitoringStation:
             return True
 
 
-
     def relative_water_level(self):
         """Returns the latest water level as a fraction of the typical range."""
         if self.typical_range_consistent() == False or self.latest_level == None:
@@ -58,13 +57,13 @@ class MonitoringStation:
             fraction = (self.latest_level - self.typical_range[0])/(self.typical_range[1] - self.typical_range[0])
             return fraction
 
-def inconsistent_typical_range_stations(self):
-    #from floodsystem.stationdata import build_station_list
-    #stations = build_station_list()
-    inconsistent_stations = []
-    for station in self:
-        if MonitoringStation.typical_range_consistent(station) == True:
-            pass
-        else:
-            inconsistent_stations.append(station)
-    return inconsistent_stations
+    def inconsistent_typical_range_stations(self):
+        #from floodsystem.stationdata import build_station_list
+        #stations = build_station_list()
+        inconsistent_stations = []
+        for station in self:
+            if MonitoringStation.typical_range_consistent(station) == True:
+                pass
+            else:
+                inconsistent_stations.append(station)
+        return inconsistent_stations
