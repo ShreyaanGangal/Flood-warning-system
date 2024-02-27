f5ebd7ef50616fb204604b656c031e2162e54aae
nat-inn <ntqi2@cam.ac.uk>
2024-02-27 16:05:00 +0000

    Task 2E Draft

---
+++ floodsystem/plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def plot_water_levels(station, dates, levels):
+    """Displays a plot of the water level data against time for a stations, and includes the typical low and high levels"""
+    typical_low_levels = station.typical_range[0]
+    typical_high_levels = station.typical_range[1]
+    plt.plot(dates, typical_low_levels, label = "Typical Low Level", ls = 'dotted')
+    plt.plot(dates, typical_high_levels, label = "Typical High Level", ls = 'dashed')
+    plt.plot(dates, levels, label = "Water levels of " + station.name)
+
+    plt.xlabel("Dates")
+    plt.ylabel("Water Level (m)")
+    plt.xticks(rotation=45)
+    plt.title("Water level of" + station.name)
+    plt.tight_layout()
+
+    plt.show()
