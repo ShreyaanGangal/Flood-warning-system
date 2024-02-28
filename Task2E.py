1e8e0d6aa560c7a65388ba13a6f53a3281f62bec
nat-inn <ntqi2@cam.ac.uk>
2024-02-28 11:44:03 +0000

    Task 2E Running (Draft)

--- Task2E.py
+++ Task2E.py
@@ -12,8 +12,10 @@ def run():
     wanted_stations = stations_highest_rel_level(stations, 5)
 
     for station in wanted_stations:
-        dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = 10))
+        dt=10
+        dt=datetime.timedelta(days=dt)
+        dates, levels = fetch_measure_levels(station.measure_id, dt)
         plot_water_levels(station, dates, levels)
 
-    if __name__ == '__main__':
-        run()
+if __name__ == '__main__':
+    run()
