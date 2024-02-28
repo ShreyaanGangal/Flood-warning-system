f2979955be72e2d6567de371509db83d4c35d9a9
Shreyaan <sg2113@cam.ac.uk>
2024-02-28 19:32:33 +0000

    updating

--- Task2G.py
+++ Task2G.py
@@ -5,10 +5,12 @@ from floodsystem.analysis import polyfit
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 import matplotlib.dates
+from floodsystem.stationdata import update_water_levels
 
 
 def run():
     stations = build_station_list()
+    update_water_levels(stations)
     monitoring_stations_ranked_by_water_level = stations_highest_rel_level(stations, len(stations))
     dict_town_and_scaled_risk = {}
     for station in monitoring_stations_ranked_by_water_level:
@@ -18,8 +20,9 @@ def run():
                 stat.measure_id, dt=datetime.timedelta(days=dt))
         p = 4
         poly, d0 = polyfit(dates, levels, p)
+        print(poly)
         derivative = poly.deriv()
-        print(derivative)
+        #print(derivative)
         a = matplotlib.dates.date2num(dates)
         #print(stat.relative_water_level())
         #print(derivative(a[-1]- d0))
@@ -28,7 +31,7 @@ def run():
         else:
             dict_town_and_scaled_risk[stat.town] = (stat.relative_water_level())*derivative(a[-1]-d0)
    
-    #print(dict_town_and_scaled_risk)
+    print(dict_town_and_scaled_risk)
 
 if __name__ == "__main__":
     print("*** Task 2G: CUED Part IA Flood Warning System ***")
