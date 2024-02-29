cf2ed75c9f5e5508d408951031a9a501c2585059
nat-inn <ntqi2@cam.ac.uk>
2024-02-29 14:14:35 +0000

    testing passed

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -7,8 +7,6 @@ def plot_water_level_with_fit(station, dates, levels, p):
     
     poly, d0 = polyfit(dates, levels, p)
 
-
-    """Displays a plot of the water level data against time for a stations, and includes the typical low and high levels"""
     typical_low_levels = station.typical_range[0]
     typical_high_levels = station.typical_range[1]
     plt.plot(dates, [typical_low_levels]*len(dates), label = "Typical Low Level", ls = 'dotted')
@@ -36,7 +34,7 @@ def plot_water_levels(station, dates, levels):
     typical_high_levels = station.typical_range[1]
     plt.plot(dates, [typical_low_levels]*len(dates), label = "Typical Low Level", ls = 'dotted')
     plt.plot(dates, [typical_high_levels]*len(dates), label = "Typical High Level", ls = 'dashed')
-    plt.plot(dates, levels, label = "Water levels of " + station.name)
+    plt.plot(dates, levels, label = station.name)
 
     plt.xlabel("Dates")
     plt.ylabel("Water Level (m)")
--- test_plot.py
+++ test_plot.py
@@ -1,8 +1,12 @@
 from floodsystem.station import MonitoringStation
 from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
 import matplotlib.pyplot as plt
+import matplotlib.dates
+import numpy as np
 
-dates = [1,2,3,4,5,6,7,8,9]
+#dates = [1,2,3,4,5,6,7,8,9]
+dates = [matplotlib.dates.date2num(date) for date in np.random.rand(9)]
+
 levels = [-4,-3,-2,-1,0,1,2,3,4]
 station = MonitoringStation("Station ID", "Measure ID", "Station", (0,-15), (-2,2),"River ", "Town")
 
@@ -14,3 +18,13 @@ def test_plot_water_levels():
     assert len(plt.gca().lines) == 3
     assert plt.gca().get_xlabel() == 'Dates'
     assert plt.gca().get_ylabel() == 'Water Level (m)'
+
+#def test_plot_water_level_with_fit():
+ #   p=4
+  #  plt.switch_backend('agg')
+   # plot_water_level_with_fit(station, dates, levels, p)
+    #axes = plt.gca()
+
+    #assert len(axes.lines) == 5
+    #assert axes.get_xlabel() == 'Dates'
+    #assert axes.get_ylabel() == 'Water Level (m)'
